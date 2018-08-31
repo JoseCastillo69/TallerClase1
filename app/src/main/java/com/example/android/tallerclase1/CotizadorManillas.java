2fc5071c27d6dcb6584213592dbc7dc0c2cc52c7
Jose <josedczarate@hotmail.com>
2018-08-30 21:19:09 -0500

    Taller1 Validado

--- app/src/main/java/com/example/android/tallerclase1/CotizadorManillas.java
+++ app/src/main/java/com/example/android/tallerclase1/CotizadorManillas.java
@@ -27,30 +27,187 @@ public class CotizadorManillas extends AppCompatActivity {
         materiales = findViewById(R.id.cmbMaterial);
         dijes = findViewById(R.id.cmbDije);
         tipos = findViewById(R.id.cmbTipo);
+        t_moneda = findViewById(R.id.cmbTipoMoneda);
         recursos = this.getResources();
 
         op1 = recursos.getStringArray(R.array.material);
-        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,op1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, op1);
         materiales.setAdapter(adapter);
 
         op2 = recursos.getStringArray(R.array.dije);
-        ArrayAdapter<String>adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,op2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, op2);
         dijes.setAdapter(adapter2);
 
         op3 = recursos.getStringArray(R.array.tipo);
-        ArrayAdapter<String>adapter3 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,op3);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, op3);
         tipos.setAdapter(adapter3);
 
         op4 = recursos.getStringArray(R.array.t_moneda);
-        ArrayAdapter<String>adapter4 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,op4);
-        t_moneda.setAdapter(adapter);
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, op4);
+        t_moneda.setAdapter(adapter4);
 
     }
 
-    public void cotizar(View v){
-        int opcion, num1, num2, resultado = 0;
+    public void cotizar(View v) {
+        int opcion1, opcion2, opcion3, opcion4, num1, resultado = 0;
+        if (validar()) {
+            opcion1 = materiales.getSelectedItemPosition();
+            opcion2 = dijes.getSelectedItemPosition();
+            opcion3 = tipos.getSelectedItemPosition();
+            opcion4 = t_moneda.getSelectedItemPosition();
+            num1 = Integer.parseInt(n1.getText().toString());
+            switch (opcion1) {
+                case 1:
+                    switch (opcion2) {
+                        case 1:
+                            switch (opcion3) {
+                                case 1:
+                                    switch (opcion4) {
+                                        case 1:
+                                            resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                            break;
+                                        case 2:
+                                            resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                            break;
+                                    }
+                                    break;
+                                case 2:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                                case 3:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                                case 4:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            switch (opcion3) {
+                                case 1:
+                                    switch (opcion4) {
+                                        case 1:
+                                            resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                            break;
+                                        case 2:
+                                            resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                            break;
+                                    }
+                                    break;
+                                case 2:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                                case 3:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                                case 4:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                            }
+
+                    }
+                case 2:
+                    switch (opcion2) {
+                        case 1:
+                            switch (opcion3) {
+                                case 1:
+                                    switch (opcion4) {
+                                        case 1:
+                                            resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                            break;
+                                        case 2:
+                                            resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                            break;
+                                    }
+                                    break;
+                                case 2:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                                case 3:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                                case 4:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            switch (opcion3) {
+                                case 1:
+                                    switch (opcion4) {
+                                        case 1:
+                                            resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                            break;
+                                        case 2:
+                                            resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                            break;
+                                    }
+                                    break;
+                                case 2:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                                case 3:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                                case 4:
+                                    resultado = Metodos.cotizar(num1, opcion1, opcion2, opcion3, opcion4);
+                                    break;
+                            }
+
+                    }
+            }
+            resumen.setText(Integer.toString(resultado));
+        }
+    }
+
+    public void limpiar(View v) {
+        n1.setText("");
+        materiales.setSelection(0);
+        dijes.setSelection(0);
+        tipos.setSelection(0);
+        t_moneda.setSelection(0);
+        n1.requestFocus();
+        resumen.setText("");
+
+    }
+
+    public boolean validar() {
+        int o1 = materiales.getSelectedItemPosition();
+        int o2 = dijes.getSelectedItemPosition();
+        int o3 = tipos.getSelectedItemPosition();
+        int o4 = t_moneda.getSelectedItemPosition();
+
+        if (n1.getText().toString().isEmpty()) {
+            n1.setError(recursos.getString(R.string.error_1));
+            n1.requestFocus();
+            return false;
+        }
+
+        if (o1 == 0) {
+            Toast.makeText(this, recursos.getString(R.string.error_2), Toast.LENGTH_SHORT).show();
+            n1.requestFocus();
+            return false;
+        }
+        if (o2 == 0) {
+            Toast.makeText(this, recursos.getString(R.string.error_3), Toast.LENGTH_SHORT).show();
+            n1.requestFocus();
+            return false;
+        }
+        if (o3 == 0) {
+            Toast.makeText(this, recursos.getString(R.string.error_4), Toast.LENGTH_SHORT).show();
+            n1.requestFocus();
+            return false;
+        }
+        if (o4 == 0) {
+            Toast.makeText(this, recursos.getString(R.string.error_5), Toast.LENGTH_SHORT).show();
+            n1.requestFocus();
+            return false;
+        }
 
 
+        return true;
 
     }
 }
+
+
--- app/src/main/java/com/example/android/tallerclase1/Metodos.java
+++ app/src/main/java/com/example/android/tallerclase1/Metodos.java
@@ -1,7 +1,121 @@
 package com.example.android.tallerclase1;
 
 public class Metodos {
-    public static int combinacion11(int cantidad){
-        return 100 * cantidad;
-    }
-}
+    public static int cotizar(int cantidad, int n1, int n2, int n3, int n4){
+        int total =0;
+        if (n1==1){
+            if (n2==1){
+                if (n3==1 || n3==2){
+                    if (n4==1){
+                        total = 100*cantidad;
+                    }
+                    if (n4==2){
+                        total = 100*3200*cantidad;
+                    }
+                    }
+                    if (n3==3){
+                        if (n4==1){
+                            total = 80*cantidad;
+                        }
+                        if (n4==2){
+                            total = 80*3200*cantidad;
+                        }
+                    }
+                    if (n3==4) {
+                        if (n4==1){
+                            total = 70*cantidad;
+                        }
+                        if (n4==2){
+                            total = 70*3200*cantidad;
+                        }
+                    }
+                    }
+                    if (n2==2) {
+                        if (n3==1 || n3==2){
+                            if (n4==1){
+                                total = 120*cantidad;
+                            }
+                            if (n4==2){
+                                total = 120*3200*cantidad;
+                            }
+                        }
+                        if (n3==3){
+                            if (n4==1){
+                                total = 100*cantidad;
+                            }
+                            if (n4==2){
+                                total = 100*3200*cantidad;
+                            }
+                        }
+                        if (n3==4) {
+                            if (n4==1){
+                                total = 90*cantidad;
+                            }
+                            if (n4==2){
+                                total = 90*3200*cantidad;
+                            }
+                        }
+                    }
+
+
+                }
+                if (n1==2) {
+                    if (n2==1){
+                        if (n3==1 || n3==2){
+                            if (n4==1){
+                                total = 90*cantidad;
+                            }
+                            if (n4==2){
+                                total = 90*3200*cantidad;
+                            }
+                        }
+                        if (n3==3){
+                            if (n4==1){
+                                total = 70*cantidad;
+                            }
+                            if (n4==2){
+                                total = 70*3200*cantidad;
+                            }
+                        }
+                        if (n3==4) {
+                            if (n4==1){
+                                total = 50*cantidad;
+                            }
+                            if (n4==2){
+                                total = 50*3200*cantidad;
+                            }
+                        }
+                    }
+                    if (n2==2) {
+                        if (n3==1 || n3==2){
+                            if (n4==1){
+                                total = 110*cantidad;
+                            }
+                            if (n4==2){
+                                total = 110*3200*cantidad;
+                            }
+                        }
+                        if (n3==3){
+                            if (n4==1){
+                                total = 90*cantidad;
+                            }
+                            if (n4==2){
+                                total = 90*3200*cantidad;
+                            }
+                        }
+                        if (n3==4) {
+                            if (n4==1){
+                                total = 80*cantidad;
+                            }
+                            if (n4==2){
+                                total = 80*3200*cantidad;
+                            }
+                        }
+                    }
+                }
+                return total;
+            }
+        }
+
+
+
